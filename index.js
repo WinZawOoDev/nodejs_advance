8cc45af2936d3ad2df9404adf2f48b58fb674083
WinZawOoDev <winzawoo.dev@gmail.com>
2024-05-12 20:27:20 +0630

    test cluster

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const path = require("node:path");
 const app = express();
 const { Worker } = require('node:worker_threads')
+const cluster = require('node:cluster')
+const { availableParallelism } = require('node:os');
+const process = require('node:process')
 
 const port = process.env.port || 8000
 
@@ -28,6 +31,22 @@ app.get('/intensive-task', (req, res) => {
 })
 
 
-app.listen(port, () => {
-    console.log(`app listening on ${port}`)
-})
+const numCpus = availableParallelism();
+if (cluster.isPrimary) {
+    console.log(`Primary process PID:${process.pid} is running`);
+
+    //For Workers
+    for (let w = 0; w < numCpus; w++) {
+        cluster.fork();
+    }
+
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`worker ${worker.process.pid} died`);
+    });
+} else {
+    app.listen(port, () => {
+        console.log(`app listening on ${port}`)
+    })
+}
+
+
